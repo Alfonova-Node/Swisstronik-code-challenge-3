214828e1bf0621731a4642f278b65d2281943770
ALFONOVA <85473027+Alfonova-Node@users.noreply.github.com>
2023-08-31 19:08:40 +0800

    Update getMessage.js

--- scripts/getMessage.js
+++ scripts/getMessage.js
@@ -12,7 +12,7 @@ const sendShieldedQuery = async (provider, destination, data) => {
 };
 
 async function main() {
-  const contractAddress = "0xf84Df872D385997aBc28E3f07A2E3cd707c9698a";
+  const contractAddress = "0x7eaf865C4e7EA00084E989b21770095105713FAf";
   const [signer] = await hre.ethers.getSigners();
   const contractFactory = await hre.ethers.getContractFactory("Swisstronik");
   const contract = contractFactory.attach(contractAddress);

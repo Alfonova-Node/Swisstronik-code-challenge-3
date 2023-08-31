82f8ecac0b15bee9e7e423697f4d9389bcdc554a
ALFONOVA <85473027+Alfonova-Node@users.noreply.github.com>
2023-08-31 21:44:19 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
+const { PRIVATE_KEY } = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
-      accounts: ["0xd5..."], //Your private key starting with "0x" 
+      accounts: [`0x${PRIVATE_KEY}`], //Your private key starting with "0x" 
       //Make sure you have enough funds in this wallet to deploy the smart contract
     },
   },
